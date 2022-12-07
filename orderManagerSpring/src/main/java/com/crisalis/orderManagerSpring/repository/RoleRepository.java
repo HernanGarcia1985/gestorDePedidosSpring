96fd8d47e4c8b599aae5a4defac5e8c2d3591dc7
HernanGarcia1985 <hernang1985@gmail.com>
2022-12-07 07:58:27 -0300

    Create RoleRepository

---
+++ orderManagerSpring/src/main/java/com/crisalis/orderManagerSpring/repository/RoleRepository.java
@@ -0,0 +1,12 @@
+package com.crisalis.orderManagerSpring.repository;
+
+import com.crisalis.orderManagerSpring.model.Role;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+
+    Role findByName(String name);
+}
