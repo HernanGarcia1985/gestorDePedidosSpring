7b94e3ebe4cdec99602995bb7df0d4e6cf0c39ee
HernanGarcia1985 <hernang1985@gmail.com>
2022-12-23 11:49:00 -0300

    Added permitAll to route /customers for test

--- orderManagerSpring/src/main/java/com/crisalis/orderManagerSpring/security/WebSecurityConfig.java
+++ orderManagerSpring/src/main/java/com/crisalis/orderManagerSpring/security/WebSecurityConfig.java
@@ -59,6 +59,7 @@ public class WebSecurityConfig {
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeRequests().antMatchers("/auth/**").permitAll()
+                .antMatchers("/customers/**").permitAll()
                 //.antMatchers("/api/test/**").permitAll()
                 .anyRequest().authenticated();
 
