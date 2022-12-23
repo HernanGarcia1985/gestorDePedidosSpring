a137800b41ba3c16e87715a9c723bf54184423bd
HernanGarcia1985 <hernang1985@gmail.com>
2022-12-23 11:40:32 -0300

    Added CustomerDto and CustomerMapper

---
+++ orderManagerSpring/src/main/java/com/crisalis/orderManagerSpring/dto/CustomerDto.java
@@ -0,0 +1,28 @@
+package com.crisalis.orderManagerSpring.dto;
+
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CustomerDto {
+
+    private String businessName;
+
+    private LocalDate startOfActivities;
+
+    private String cuit;
+
+    private String address;
+
+    private String phone;
+
+    private String email;
+
+    private String customerType;
+
+}
---
+++ orderManagerSpring/src/main/java/com/crisalis/orderManagerSpring/service/mapper/CustomerMapper.java
@@ -0,0 +1,31 @@
+package com.crisalis.orderManagerSpring.service.mapper;
+
+import com.crisalis.orderManagerSpring.dto.CustomerDto;
+import com.crisalis.orderManagerSpring.model.Company;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomerMapper {
+
+    public CustomerDto companyToDto (Company company){
+        return CustomerDto.builder()
+                .businessName(company.getBusinessName())
+                .startOfActivities(company.getStartOfActivities())
+                .cuit(company.getCuit())
+                //.address(company.getAdress())
+                //.phone(company.getPhone())
+                //.email(company.getEmail())
+                .build();
+    }
+
+    public Company companyDtoToEntity (CustomerDto customerDto){
+        return Company.builder()
+                .businessName(customerDto.getBusinessName())
+                .startOfActivities(customerDto.getStartOfActivities())
+                .cuit(customerDto.getCuit())
+                //.address(customerDto.getAddress())
+                //.phone(customerDto.getPhone())
+                //.email(customerDto.getEmail())
+                .build();
+    }
+}
