8b04c547714b108f98e53551133b7915499a0ef9
HernanGarcia1985 <hernang1985@gmail.com>
2023-01-02 02:51:04 -0300

    CreateCustomer logic added

--- order-manager-react/src/components/CustomerForm.jsx
+++ order-manager-react/src/components/CustomerForm.jsx
@@ -1,31 +1,64 @@
 import { Container, Row, Col } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import { useState } from 'react';
+import createCustomer from '../utils/createCustomer';
 
 const CustomerForm = () => {
+
+    const [customerType, setCustomerType] = useState('')
+    const [businessName, setBusinessName] = useState('')
+    const [startOfActivities, setStartOfActivities] = useState('')
+    const [cuit, setCuit] = useState('')
+    const [name, setName] = useState('')
+    const [lastName, setLastName] = useState('')
+    const [dni, setDni] = useState('')
+    const [email, setEmail] = useState('')
+    const [phone, setPhone] = useState('')
+    const [address, setAddress] = useState('')
+
+    const create = (e) => {
+        e.preventDefault()
+        if (!noValidate()){
+            createCustomer(customerType, businessName, startOfActivities, cuit, name, lastName, dni, email, phone, address)
+        } else {
+            alert("Please check the data entered and complete all necessary fields");
+        }    
+    }
+
+    const noValidate = () =>{
+        if (customerType.toLowerCase() ==='person'){
+            return !(name.length && lastName.length && dni.length && email.includes('@') && phone.length && address.length)    
+        } else if (customerType.toLowerCase() ==='company') {
+            return !(businessName.length && startOfActivities.length && cuit.length && name.length && lastName.length && dni.length && email.includes('@') && phone.length && address.length)    
+        } else {
+            return true
+        }
+    }
+
   return (
     <>
     <Container>
     <Form >
         <Form.Group className="mb-3 " >
-            <Form.Check inline type="radio" name="PersonOrCompany" label="Person"  value = "Person" />
-            <Form.Check inline type="radio" name="PersonOrCompany" label="Company" value = "Company" />
+            <Form.Check inline type="radio" name="PersonOrCompany" label="Person"  value = "Person" onChange={(e) => { setCustomerType(e.target.value)}}/>
+            <Form.Check inline type="radio" name="PersonOrCompany" label="Company" value = "Company" onChange={(e) => { setCustomerType(e.target.value)}}/>
         </Form.Group>
         <Form.Group className="mb-3">
             <Form.Label>Business Name</Form.Label>
-            <Form.Control placeholder="Business Name" />
+            <Form.Control placeholder="Business Name" onChange={(e) => { setBusinessName(e.target.value)}}/>
         </Form.Group>
         <Row>
             <Col md-auto>
                 <Form.Group className="mb-3">
                     <Form.Label>Start Of Activities</Form.Label>
-                    <Form.Control type="date" placeholder="Start Of Activities" />
+                    <Form.Control type="date" placeholder="Start Of Activities" onChange={(e) => { setStartOfActivities(e.target.value)}}/>
                 </Form.Group>
             </Col>
             <Col md-auto>
                 <Form.Group className="mb-3">
                     <Form.Label>CUIT</Form.Label>
-                    <Form.Control placeholder="Number of CUIT" />
+                    <Form.Control placeholder="Number of CUIT" onChange={(e) => { setCuit(e.target.value)}}/>
                 </Form.Group>
             </Col>
         </Row>
@@ -33,19 +66,19 @@ const CustomerForm = () => {
             <Col>
                 <Form.Group className="mb-3">
                     <Form.Label>Name</Form.Label>
-                    <Form.Control placeholder="Name" />
+                    <Form.Control placeholder="Name" onChange={(e) => { setName(e.target.value)}}/>
                 </Form.Group>
             </Col>
             <Col>
                 <Form.Group className="mb-3">
                     <Form.Label>Last Name</Form.Label>
-                    <Form.Control placeholder="Last Name" />
+                    <Form.Control placeholder="Last Name" onChange={(e) => { setLastName(e.target.value)}}/>
                 </Form.Group>
             </Col>
             <Col>
                 <Form.Group className="mb-3">
                     <Form.Label>DNI</Form.Label>
-                    <Form.Control placeholder="Number of DNI, CI or ID" />
+                    <Form.Control placeholder="Number of DNI, CI or ID" onChange={(e) => { setDni(e.target.value)}}/>
                 </Form.Group>
             </Col>
         </Row>
@@ -53,22 +86,22 @@ const CustomerForm = () => {
             <Col>
                 <Form.Group className="mb-3">
                     <Form.Label>Email</Form.Label>
-                    <Form.Control placeholder="Contact Email" />
+                    <Form.Control placeholder="Contact Email" onChange={(e) => { setEmail(e.target.value)}}/>
                 </Form.Group>
             </Col>
             <Col>
                 <Form.Group className="mb-3">
                     <Form.Label>Phone</Form.Label>
-                    <Form.Control placeholder="Contact Phone" />
+                    <Form.Control placeholder="Contact Phone" onChange={(e) => { setPhone(e.target.value)}}/>
                 </Form.Group>
             </Col>
         </Row>
             <Form.Group className="mb-3">
                 <Form.Label>Address</Form.Label>
-                <Form.Control placeholder="Address" />
+                <Form.Control placeholder="Address" onChange={(e) => { setAddress(e.target.value)}}/>
             </Form.Group>
         
-        <Button type="submit">Create</Button>
+        <Button onClick={create}>Create</Button>
     </Form>
     </Container>
     </>
---
+++ order-manager-react/src/utils/createCustomer.jsx
@@ -0,0 +1,51 @@
+const createCustomer = async (customerType, businessName, startOfActivities, cuit, name, lastName, dni, email, phone, address) => {
+    
+    console.log (customerType, businessName, startOfActivities, cuit, name, lastName, dni, email, phone, address)
+    
+    let url = 'http://localhost:8080/customers/';
+
+    let newCustomer = {
+        customerType: customerType,
+        businessName: businessName,
+        startOfActivities: startOfActivities,
+        cuit: cuit,
+        name: name,
+        lastName: lastName,
+        dni: dni,
+        email: email,
+        phone: phone,
+        address: address
+    }
+
+    let data = {
+        method: 'POST',
+        //mode: 'no-cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newCustomer)
+    }
+    
+    try {
+        const result = await fetch(url, data)
+            .then(response => {
+                if(response.ok){
+                    return response.json()
+                } else {
+                    console.log("error")
+                }
+                })
+            .then(data => {
+                console.log(data)
+                if(data.exception) {
+                    alert("Failed to create a new customer. Error: " + data.exception);
+                } else {
+                    alert("Customer was created successfully!");
+                }    
+            });        
+    } catch (error) {
+        alert("Something went wrong trying to create a new customer. Please try again. Error: "+ error);                        
+    }
+}
+
+export default createCustomer
