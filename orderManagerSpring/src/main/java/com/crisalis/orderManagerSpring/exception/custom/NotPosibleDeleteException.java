f84a878d6178f840911838ddb7b2ecdec1aadb98
HernanGarcia1985 <hernang1985@gmail.com>
2023-01-02 02:44:57 -0300

    [FIX] Description fixed.

--- orderManagerSpring/src/main/java/com/crisalis/orderManagerSpring/exception/custom/NotPosibleDeleteException.java
+++ orderManagerSpring/src/main/java/com/crisalis/orderManagerSpring/exception/custom/NotPosibleDeleteException.java
@@ -2,7 +2,7 @@ package com.crisalis.orderManagerSpring.exception.custom;
 
 public class NotPosibleDeleteException extends RuntimeException{
 
-    private static final String DESCRIPTION = "Not posible (400)";
+    private static final String DESCRIPTION = "Could not delete (400)";
 
     public NotPosibleDeleteException(String error){
         super(DESCRIPTION + ". " + error);
