893bfe08f5de72787aafda36686d37b5d775a63e
HernanGarcia1985 <hernang1985@gmail.com>
2023-01-18 09:57:08 -0300

    OrderDetailController was renamed to OrderController

--- orderManagerSpring/src/main/java/com/crisalis/orderManagerSpring/controller/OrderController.java
+++ orderManagerSpring/src/main/java/com/crisalis/orderManagerSpring/controller/OrderController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/orders")
-public class OrderAssetDetailController {
+public class OrderController {
 
     @Autowired
     OrderAssetDetailServiceImpl orderAssetDetailServiceImpl;
