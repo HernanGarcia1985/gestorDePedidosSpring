d4338cb10990926331c25e7de11e57ab60db2038
HernanGarcia1985 <hernang1985@gmail.com>
2023-02-02 16:35:57 -0300

    Removed search bar, unused options and some more modifications.

--- order-manager-react/src/components/NavbarCustom.jsx
+++ order-manager-react/src/components/NavbarCustom.jsx
@@ -4,17 +4,22 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
+import { useState } from 'react';
 
 const NavbarCustom = () => {
 
-    let user = localStorage.getItem('userLogged') ? localStorage.getItem('userLogged') : '';
+    const [user, setUser] = useState(localStorage.getItem('userLogged') ? localStorage.getItem('userLogged') : '');
 
     let admin = (!user || !(JSON.parse(user).roles[0]==="ADMIN")) ? false : true;
+
+    const welcome = {
+        fontWeight: "bold"
+    }
 
     const signoff = () => {
         if(user){
             localStorage.removeItem('userLogged')
+            setUser('')
             alert('Logged out successfully.')
         }    
     }
@@ -28,33 +33,29 @@ const NavbarCustom = () => {
                 <Nav className="me-auto">
                     <NavDropdown title="Orders" id="basic-nav-dropdown">
                     <NavDropdown.Item href="/orders/create">Create new Order</NavDropdown.Item>
-                    <NavDropdown.Item href="/orders/show">View an Order</NavDropdown.Item>
                     <NavDropdown.Divider />
                     <NavDropdown.Item href="/orders/">View all Orders</NavDropdown.Item>
                     </NavDropdown>
                     <NavDropdown title="Customers" id="basic-nav-dropdown">
                     <NavDropdown.Item href="/customers/create">Create a Customer</NavDropdown.Item>
-                    <NavDropdown.Item href="/customers/show">View a Customer</NavDropdown.Item>
                     <NavDropdown.Divider />
                     <NavDropdown.Item href="/customers/">View all Customers</NavDropdown.Item>
                     </NavDropdown>
                     <NavDropdown title="Assets" id="basic-nav-dropdown">
-                    <NavDropdown.Item href="/assets/create">Create new Asset</NavDropdown.Item>
-                    <NavDropdown.Item href="/assets/show">View an Asset</NavDropdown.Item>
+                        {admin ? <NavDropdown.Item href="/assets/create">Create new Asset</NavDropdown.Item> : null }
                     <NavDropdown.Divider />
                     <NavDropdown.Item href="/assets/">View all Assets</NavDropdown.Item>
                     </NavDropdown>
                     <NavDropdown title="Taxes" id="basic-nav-dropdown">
                         {admin ? <NavDropdown.Item href="/taxes/create">Create new Tax</NavDropdown.Item> : null}
-                        {admin ? <NavDropdown.Item href="/taxes/show">View a Tax</NavDropdown.Item> : null}
                     <NavDropdown.Divider />
                         <NavDropdown.Item href="/taxes/">View all Taxes</NavDropdown.Item>
                     </NavDropdown>
-                    <NavDropdown title="Reports" id="basic-nav-dropdown">
-                        {admin ? <NavDropdown.Item href="/reports/historicalOrders">Historical Orders</NavDropdown.Item> : null}
-                        {admin ? <NavDropdown.Item href="/reports/biggestDiscount">Biggest Discount</NavDropdown.Item> : null}
-                        {admin ? <NavDropdown.Item href="/reports/totalDiscount">Total Discount</NavDropdown.Item> : null}
-                    </NavDropdown>
+                    {admin ? <NavDropdown title="Reports" id="basic-nav-dropdown">
+                        <NavDropdown.Item href="/reports/historicalOrders">Historical Orders</NavDropdown.Item>
+                        <NavDropdown.Item href="/reports/biggestDiscount">Biggest Discount</NavDropdown.Item>
+                        <NavDropdown.Item href="/reports/totalDiscount">Total Discount</NavDropdown.Item>
+                    </NavDropdown> : null }
                     <NavDropdown title="Users" id="basic-nav-dropdown">
                     {user ? null : <NavDropdown.Item href="/auth/signin">SignIn</NavDropdown.Item>}
                     <NavDropdown.Item href="/auth/signup">SignUp</NavDropdown.Item>
@@ -62,16 +63,9 @@ const NavbarCustom = () => {
                     </NavDropdown>    
                 </Nav>
                 <Form className="d-flex">
-                    <Container>
-                        {user ? <Form.Label >Welcome {JSON.parse(user).username}</Form.Label> : null}
+                    <Container key={user}>
+                        {user ? <Form.Label style={welcome} >Welcome {JSON.parse(user).username}</Form.Label> : null}
                     </Container>
-                <Form.Control
-                    type="search"
-                    placeholder="Search"
-                    className="me-2"
-                    aria-label="Search"
-                        />
-                    <Button className="btn btn-secondary my-2 my-sm-0" type="submit">Search</Button>
                 </Form>
                 </Navbar.Collapse>    
             </Container>
