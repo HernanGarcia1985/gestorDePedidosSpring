83aa690a543db3ccd2003e11747a2d3cf2749aee
HernanGarcia1985 <hernang1985@gmail.com>
2023-02-03 12:35:09 -0300

    Added the corsFilter bean to solve the cors problem.

--- orderManagerSpring/src/main/java/com/crisalis/orderManagerSpring/OrderManagerSpringApplication.java
+++ orderManagerSpring/src/main/java/com/crisalis/orderManagerSpring/OrderManagerSpringApplication.java
@@ -2,14 +2,37 @@ package com.crisalis.orderManagerSpring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.crisalis")
+//@ComponentScan(basePackages = "com.crisalis")
 public class OrderManagerSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrderManagerSpringApplication.class, args);
+	}
+
+	@Bean
+	public CorsFilter corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.setAllowedOriginPatterns(Arrays.asList(
+				""));
+		config.addAllowedHeader("");
+		config.addAllowedMethod("OPTIONS");
+		config.addAllowedMethod("GET");
+		config.addAllowedMethod("POST");
+		config.addAllowedMethod("PUT");
+		config.addAllowedMethod("DELETE");
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter(source);
 	}
 
 }
